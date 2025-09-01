5f66e0f54dc88e3e47eaa90c2bff5aedd632c233
Parisa <fairyahmadi71@gmail.com>
2025-09-01 21:27:49 +0330

    Add RabbitMQ producer module and service

--- producer-app/src/app.module.ts
+++ producer-app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProducerModule } from './producer/producer.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ProducerModule],
   controllers: [AppController],
   providers: [AppService],
 })
