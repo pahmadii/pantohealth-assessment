5aad977839be52c7d21f37f7ac8cd4891d47f113
Parisa <fairyahmadi71@gmail.com>
2025-09-01 21:30:05 +0330

    Add producer controller with test endpoint for sending sample data

--- producer-app/src/producer/producer.module.ts
+++ producer-app/src/producer/producer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProducerService } from './producer.service';
+import { ProducerController } from './producer.controller';
 
 @Module({
-  providers: [ProducerService]
+  providers: [ProducerService],
+  controllers: [ProducerController]
 })
 export class ProducerModule {}
